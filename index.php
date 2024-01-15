94c6f4c5509498fde8725409156ac53d98af2500
elisabichiappini <elisabichiappini@gmail.com>
2024-01-15 22:41:48 +0100

    feat: scaffolding genre with methods

--- index.php
+++ index.php
@@ -2,10 +2,14 @@
 require_once __DIR__ . '/models/Movie.php';
 // istanzio due classi Movie, Inception e DallasBuyerClub
 $Inception = new Movie();
+$Inception->setGenre('Action');
+
 $DallasBuyerClub = new Movie();
+$DallasBuyerClub->setGenre('Action/Adventure');
 
 echo "<pre>";
 var_dump($Inception);
 var_dump($DallasBuyerClub);
 echo "</pre>";
+
 ?>
--- models/Movie.php
+++ models/Movie.php
@@ -1,10 +1,62 @@
 <?php
 // definisco la classe Movie
 class Movie {
- // dichiarazione variabili di istanza
- public $title;
- public $comingDate;
- public $author;
- public $genre;
+    // dichiarazione variabili di istanza
+    public $title;
+    public $comingDate;
+    public $author;
+    public $genre;
+
+    // per TITLE dichiaro metodo della classe Movie setter
+    public function setTitle($title) {
+        if($title !== '') {
+            $this->title = $title;
+        } else {
+            $this->title = 'NotFound';
+        }
+    }
+    // per TITLE dichiaro metodo della classe Movie getter
+    public function getTitle() {
+        return $this->title;
+    }
+
+    // per COMINGDATE dichiaro metodo della classe Movie setter
+    public function setDateComing($date) {
+        if($date !== '') {
+            $this->comingDate = $date;
+        } else {
+            $this->comingDate = 'NotFound';
+        }
+    }
+    // per COMINGDATE dichiaro metodo della classe Movie getter
+    public function getDate() {
+        return $this->comingDate;
+    }
+
+    // per AUTHOR dichiaro metodo della classe Movie setter
+    public function setAuthor($name) {
+        if($name !== '') {
+            $this->author = $name;
+        } else {
+            $this->author = 'NotFound';
+        }
+    }
+    // per AUTHOR dichiaro metodo della classe Movie getter
+    public function getAuthor() {
+        return $this->author;
+    }
+    
+    // per GENRE dichiaro metodo della classe Movie setter
+    public function setGenre($type) {
+        if($type !== '') {
+            $this->genre = $type;
+        } else {
+            $this->genre = 'NotFound';
+        }
+    }
+    // per GENRE dichiaro metodo della classe Movie getter
+    public function getGenre() {
+        return $this->genre;
+    }
 };
 ?>
